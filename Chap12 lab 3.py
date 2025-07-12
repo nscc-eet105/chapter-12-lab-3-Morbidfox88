395a28ccfefec0855d83f4f67077cd082c13dba4
Chad Collard <chadcollard1567@gmail.com>
2025-07-12 11:38:04 -0400

    chapter 12 lab 3 done

---
+++ Chap12 lab 3.py
@@ -0,0 +1,22 @@
+#Chad Collard
+#Chapter 12 Lab 3
+#7/12/2025
+#Weather app
+
+
+import requests
+
+# https://api.openweathermap.org/data/2.5/weather?zip=43512,us&appid=80e3bd3ae92a57f438be3823b5d9d234
+
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?zip=43512,us&units=imperial&appid=80e3bd3ae92a57f438be3823b5d9d234")
+
+if response:
+    response_dict = response.json()
+    print('The Current Weather for', response_dict['name'],':')
+    #print('Conditions:', response_dict['weather'] ['description'])
+    print('Temperature:', response_dict['main'] ['temp'], 'degrees')
+    print('Feels Like:', response_dict['main'] ['feels_like'], 'degrees')
+    print('Humidity:', response_dict['main'] ['humidity'],'%')
+    print('Wind:', response_dict['wind']['speed'], 'knots @', response_dict['wind']['deg'], 'degrees')
+
+    
